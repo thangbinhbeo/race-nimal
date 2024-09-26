d15c741e0b0424d1f27a2bfa613d5129acecce8e
thangbinhbeo <binhbeopro1122@gmail.com>
2024-09-27 05:04:34 +0700

    "sao ke"

--- app/src/main/java/com/example/prm392_miniproject_racenimal/HomeScreen.java
+++ app/src/main/java/com/example/prm392_miniproject_racenimal/HomeScreen.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class WalletActivity extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.home_game);
     }
 }
